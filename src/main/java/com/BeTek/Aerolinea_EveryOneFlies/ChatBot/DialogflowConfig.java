1e2d0240f4dfec7548ae7daae64c1dfb3c211276
GinaLoaizaH <ginita011295@gmail.com>
2024-07-28 19:06:46 -0500

    Config env

--- src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/DialogflowConfig.java
+++ src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/DialogflowConfig.java
@@ -10,20 +10,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 
 @Configuration
-
 public class DialogflowConfig {
 
     private static final Dotenv dotenv = Dotenv.load();
 
     @Bean
     public SessionsClient sessionsClient() throws IOException {
-        String credentialsPath = dotenv.get("GOOGLE_APPLICATION_CREDENTIALS");
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+        String credentialsJson = dotenv.get("GOOGLE_APPLICATION_CREDENTIALS");
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsJson));
+
         SessionsSettings sessionsSettings = SessionsSettings.newBuilder()
                 .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
                 .build();
