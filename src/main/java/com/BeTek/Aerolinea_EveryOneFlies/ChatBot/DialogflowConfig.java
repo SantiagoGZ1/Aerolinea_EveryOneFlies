9d804cef9bb37a1c08869724f18e52e2ff9bff50
GinaLoaizaH <ginita011295@gmail.com>
2024-07-29 07:37:59 -0500

    Modificando env y gitIgnore

--- src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/DialogflowConfig.java
+++ src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/DialogflowConfig.java
@@ -6,14 +6,12 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.dialogflow.v2.*;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+
 
 
 @Configuration
