ab624fc720b8e8312e272d70a070a528eda53f75
GinaLoaizaH <ginita011295@gmail.com>
2024-07-29 11:20:36 -0500

    Solo es de prueba

--- src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/DialogflowConfig.java
+++ src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/DialogflowConfig.java
@@ -20,9 +20,12 @@ public class DialogflowConfig {
 
     @Bean
     public SessionsClient sessionsClient() throws IOException {
-        String credentialsPath = "src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/everyoneflies-ewlv-bb71cf9941cb.json";
+        String credentialsJson = System.getenv("GOOGLE_CREDENTIALS");
 
-        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
+        if (credentialsJson == null || credentialsJson.isEmpty()) {
+            throw new IllegalStateException("GOOGLE_CREDENTIALS environment variable is not set");
+        }
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new ByteArrayInputStream(credentialsJson.getBytes()));
         SessionsSettings sessionsSettings = SessionsSettings.newBuilder()
                 .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
                 .build();
