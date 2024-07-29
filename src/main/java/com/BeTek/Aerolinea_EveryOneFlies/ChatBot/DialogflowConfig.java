deb61e7ac727e948b42f2634b02e79629cc6ca98
GinaLoaizaH <ginita011295@gmail.com>
2024-07-29 12:53:27 -0500

    Este si es

--- src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/DialogflowConfig.java
+++ src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/DialogflowConfig.java
@@ -8,10 +8,11 @@ import com.google.cloud.dialogflow.v2.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
+import java.nio.charset.StandardCharsets;
 
 
 @Configuration
@@ -19,7 +20,11 @@ public class DialogflowConfig {
 
     @Bean
     public SessionsClient sessionsClient() throws IOException {
-        InputStream inputStream = new FileInputStream("src/main/java/com/BeTek/Aerolinea_EveryOneFlies/ChatBot/everyoneflies-ewlv-bb71cf9941cb.json");
+        String credentialsJson = System.getenv("GOOGLE_CREDENTIALS");
+        if (credentialsJson == null) {
+            throw new IllegalStateException("GOOGLE_CREDENTIALS environment variable is not set");
+        }
+        InputStream inputStream = new ByteArrayInputStream(credentialsJson.getBytes(StandardCharsets.UTF_8));
         GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
         return SessionsClient.create(SessionsSettings.newBuilder()
                 .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
